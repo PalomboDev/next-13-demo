7cea4c488e37e4efa99aff5004924d93ebbc2d52
Colin Palombo <colin.palombo@yahoo.com>
2022-11-19 21:18:24 -0600

    Fix text for server box

--- components/diagram/ServerBox.tsx
+++ components/diagram/ServerBox.tsx
@@ -14,11 +14,8 @@ export default async function ServerBox({ children, hint }: ServerBoxProps ) {
         <div className={"bg-orange-600 p-5 border-2 border-black"}>
             <div className={"mb-5"}>Server</div>
 
-            {
-                hint &&
-                <div className={"text-lg"}>This user's info was loaded (on the server) before it's parent client component was rendered in.</div> &&
-                <div className={"text-lg"}>This is why as soon as the parent client component is rendered in, the user info is already avaiable</div>
-            }
+            {hint && <div className={"text-lg"}>This user's info was loaded (on the server) before it's parent client component was rendered in.</div>}
+            {hint && <div className={"text-lg"}>This is why as soon as the parent client component is rendered in, the user info is already available.</div>}
 
             <div className={"border-gray-700 border-2 w-fit m-auto my-5"}>
                 <div className={"text-2xl text-gray-300 m-2"}>{data.name}</div>
